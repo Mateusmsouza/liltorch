f52a3ab3df444d45bb96ef6e5ec01ae1c31795e2
Mateus Souza <mateussouza@luizalabs.com>
2024-06-21 17:45:09 -0300

    add cd to ci

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liltorch',
+    version='0.0.1',
+    description='Small neural network library made only with raw python',
+    author='Mateus Souza',
+    packages=find_packages(),
+)
