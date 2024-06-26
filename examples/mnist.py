394a6f428d6cd8a76ceaf60d86b9244844d2e2bd
Mateus Souza <mateussouza@luizalabs.com>
2024-06-26 20:08:05 -0300

    updat

--- examples/mnist.py
+++ examples/mnist.py
@@ -16,15 +16,15 @@ import numpy as np
 # load MNIST from server
 lr = 0.1
 epochs = 10
-bath_size = 16
+batch_size = 16
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(x_train.shape[0], 1, 28*28)
+x_train = x_train.reshape(x_train.shape[0], 28*28)
 x_train = x_train.astype('float32')
 x_train /= 255
 y_train = to_categorical(y_train)
 
-x_test = x_test.reshape(x_test.shape[0], 1, 28*28)
+x_test = x_test.reshape(x_test.shape[0], 28*28)
 x_test = x_test.astype('float32')
 x_test /= 255
 y_test = to_categorical(y_test)
@@ -43,29 +43,47 @@ model.add(ActivationLayerTanh())
 for epoch in range(epochs):
     epoch_loss = 0
     dataset_size = len(x_train)
-    for i in range(dataset_size):
-        sample = x_train[i]
-        target = np.expand_dims(y_train[i], axis=0)
+    batch_begin = 0
+    batch_end = batch_size - 1
+    while batch_begin < dataset_size:
+        data = x_train[batch_begin:batch_end]
+        target = y_train[batch_begin:batch_end]
 
         # forward
-        output = model.forward(sample)
+        output = model.forward(data)
         epoch_loss += mse_loss(target, output)
 
         # backward pass
         error = mse_grad(target, output)
         model.backward(error)
 
+        batch_begin += batch_size
+        batch_end += batch_size
+        batch_end = min(dataset_size, batch_end)
+
     print(f'Epoch {epoch} -> Average loss {epoch_loss/len(x_train)}')
 
 # testing
 correct = 0
 total = 0
-for i in range(len(x_test)):
-    sample = x_test[i]
-    target = np.expand_dims(y_test[i], axis=0)
+dataset_size = len(x_test)
+batch_begin = 0
+batch_end = batch_size - 1
 
-    output = model.forward(sample)
+while batch_begin < dataset_size -1:
+    data = x_test[batch_begin:batch_end]
+    target = y_test[batch_begin:batch_end]
 
-    correct += int(np.argmax(output) == np.argmax(target))
+    output = model.forward(data)
+
+    correct += np.all(np.array(output) == np.array(target), axis=1).sum()
+    #int(np.argmax(output) == np.argmax(target))
     total += 1
+
+    batch_begin += batch_size
+    batch_end += batch_size
+    batch_end = min(dataset_size, batch_end)
+
+
+print(f"correct {correct}")
 print(f'Test Accuracy: {correct/total}')
