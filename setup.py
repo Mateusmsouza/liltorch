4d9349515c8cc8ca53537bf536b082b3891d7abc
Mateus <mateus_msouza@outlook.com>
2024-06-27 21:13:07 -0300

    fix

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     description='Small neural network library made only with raw python',
     author='Mateus Souza',
     packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
